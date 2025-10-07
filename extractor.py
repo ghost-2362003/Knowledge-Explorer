1eb5549633e3bc9d43879a8c4e188eff82387e00
Shubhojyoti Das <shubhojyotidas800@gmail.com>
2025-10-07 17:15:39 +0000

    modify extractor.py

--- extractor.py
+++ extractor.py
@@ -1,37 +1,45 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import pandas as pd
 
-## Create a SPARQL endpoint
-sparql = SPARQLWrapper("https://dbpedia.org/sparql")
-sparql.setReturnFormat(JSON)
+def extract_data():
+    ## Create a SPARQL endpoint
+    sparql = SPARQLWrapper("https://dbpedia.org/sparql")
+    sparql.setReturnFormat(JSON)
 
-## Create the query
-query = '''
-SELECT ?person ?personLabel ?birthPlace ?birthPlaceLabel
-WHERE{
-    ?person a dbo:Person;
-            dbo:birthPlace ?birthPlace.
-    ?person rdfs:label ?personLabel.
-    ?birthPlace rdfs:label ?birthPlaceLabel.
-    FILTER (lang(?personLabel) = "en")
-    FILTER (lang(?birthPlaceLabel) = "en")
-}
-LIMIT 200
-'''
+    ## Create the query
+    query = '''
+    SELECT ?person ?personLabel ?birthPlace ?birthPlaceLabel
+    WHERE{
+        ?person a dbo:Person;
+                dbo:birthPlace ?birthPlace.
+        ?person rdfs:label ?personLabel.
+        ?birthPlace rdfs:label ?birthPlaceLabel.
+        FILTER (lang(?personLabel) = "en")
+        FILTER (lang(?birthPlaceLabel) = "en")
+    }
+    LIMIT 200
+    '''
 
-## Send the query to the endpoint
-sparql.setQuery(query=query)
+    ## Send the query to the endpoint
+    sparql.setQuery(query=query)
 
-## Get the results
-results = sparql.query().convert()
+    ## Get the results
+    results = sparql.query().convert()
 
-## Parse through the results
-data = []
-for res in results["results"]["bindings"]:
-    data.append({
-        "Person": res["personLabel"]["value"],
-        "BirthPlace": res["birthPlaceLabel"]["value"]
-    })
+    ## Parse through the results
+    data = []
+    for res in results["results"]["bindings"]:
+        data.append({
+            "Person": res["personLabel"]["value"],
+            "BirthPlace": res["birthPlaceLabel"]["value"]
+        })
     
-df = pd.DataFrame(data)
-print(df.head())
+    return pd.DataFrame(data)
+
+def transform_to_sentence(df):
+    sentences = []
+    for index, row in df.iterrows():
+        sentence = f"{row['Person']} was born in {row['BirthPlace']}."
+        sentences.append(sentence)
+    
+    return sentences
